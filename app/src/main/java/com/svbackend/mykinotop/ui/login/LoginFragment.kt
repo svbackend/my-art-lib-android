b1ae8b5a32345b7648ff79ec1041347d25b55561
Valentyn Saik <svbackend22@gmail.com>
2019-07-24 12:43:26 +0200

    Added basic view for login screen

--- app/src/main/java/com/svbackend/mykinotop/ui/login/LoginFragment.kt
+++ app/src/main/java/com/svbackend/mykinotop/ui/login/LoginFragment.kt
@@ -2,6 +2,7 @@ package com.svbackend.mykinotop.ui.login
 
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -40,5 +41,9 @@ class LoginFragment : ScopedFragment(), KodeinAware {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(LoginViewModel::class.java)
+    }
+
+    public fun loginBtnOnClick(view: View) {
+        Log.e("test", "clicked")
     }
 }
