c1d4351bc6602ed283aad48c00d88461d4cc3f3c
Valentyn Saik <svbackend22@gmail.com>
2019-07-24 09:07:48 +0200

    Data source classes added

--- app/src/main/java/com/svbackend/mykinotop/api/UserDataSource.kt
+++ app/src/main/java/com/svbackend/mykinotop/api/UserDataSource.kt
@@ -1,2 +1,12 @@
 package com.svbackend.mykinotop.api
 
+import androidx.lifecycle.LiveData
+import com.svbackend.mykinotop.dto.user.User
+
+interface UserDataSource {
+    val currentUser: LiveData<User>
+
+    suspend fun fetchCurrentUser(
+        id: Int
+    )
+}
--- app/src/main/java/com/svbackend/mykinotop/api/UserDataSourceImpl.kt
+++ app/src/main/java/com/svbackend/mykinotop/api/UserDataSourceImpl.kt
@@ -1,2 +1,18 @@
-package com.svbackend.mykinotop.api 
+package com.svbackend.mykinotop.api
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.svbackend.mykinotop.dto.user.User
+
+class UserDataSourceImpl(
+    private val apiService: ApiService
+) : UserDataSource {
+
+    private val _currentUser = MutableLiveData<User>()
+    override val currentUser: LiveData<User>
+        get() = _currentUser
+
+    override suspend fun fetchCurrentUser(id: Int) {
+        // todo
+    }
+}
