2bbd23a56a8c5ce3e6d4e56a35ee5339a4745368
Valentyn Saik <svbackend22@gmail.com>
2019-07-24 11:52:43 +0200

    Login Fragment - removed all unused imports

--- app/src/main/java/com/svbackend/mykinotop/ui/login/LoginFragment.kt
+++ app/src/main/java/com/svbackend/mykinotop/ui/login/LoginFragment.kt
@@ -11,6 +11,7 @@ import com.svbackend.mykinotop.R
 import com.svbackend.mykinotop.api.ApiService
 import com.svbackend.mykinotop.dto.login.Credentials
 import com.svbackend.mykinotop.dto.login.LoginRequest
+import com.svbackend.mykinotop.ui.ScopedFragment
 import kotlinx.android.synthetic.main.login_fragment.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
@@ -18,16 +19,14 @@ import kotlinx.coroutines.launch
 import org.kodein.di.Kodein
 import org.kodein.di.KodeinAware
 import org.kodein.di.android.x.closestKodein
+import org.kodein.di.generic.instance
 
-class LoginFragment : Fragment(), KodeinAware {
+class LoginFragment : ScopedFragment(), KodeinAware {
 
     override val kodein by closestKodein()
+    private val apiService: ApiService by instance()
 
     // todo create ViewModelFactory
-
-    companion object {
-        fun newInstance() = LoginFragment()
-    }
 
     private lateinit var viewModel: LoginViewModel
 
@@ -41,11 +40,5 @@ class LoginFragment : Fragment(), KodeinAware {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(LoginViewModel::class.java)
-
-
-    }
-
-    private fun bindUI() {
-        
     }
 }
