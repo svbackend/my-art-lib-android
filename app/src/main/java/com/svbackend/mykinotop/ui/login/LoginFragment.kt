df725e057544898492a4237ba83bfadbbe0c7593
Valentyn Saik <svbackend22@gmail.com>
2019-07-26 12:58:15 +0200

    Handle auth error and display message

--- app/src/main/java/com/svbackend/mykinotop/ui/login/LoginFragment.kt
+++ app/src/main/java/com/svbackend/mykinotop/ui/login/LoginFragment.kt
@@ -3,22 +3,28 @@ package com.svbackend.mykinotop.ui.login
 import android.content.Intent
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.svbackend.mykinotop.R
 import com.svbackend.mykinotop.api.ApiService
 import com.svbackend.mykinotop.db.User
 import com.svbackend.mykinotop.db.UserDao
 import com.svbackend.mykinotop.dto.login.Credentials
 import com.svbackend.mykinotop.dto.login.LoginRequest
+import com.svbackend.mykinotop.dto.login.LoginSuccessResponse
 import com.svbackend.mykinotop.ui.MoviesSearchActivity
 import com.svbackend.mykinotop.ui.ScopedFragment
 import kotlinx.android.synthetic.main.login_fragment.*
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
 import org.kodein.di.KodeinAware
 import org.kodein.di.android.x.closestKodein
 import org.kodein.di.generic.instance
+import retrofit2.HttpException
 
 class LoginFragment : ScopedFragment(), KodeinAware {
 
@@ -53,11 +59,23 @@ class LoginFragment : ScopedFragment(), KodeinAware {
         showLoading()
 
         val username = fieldUsername.text.toString()
-        val password = fieldUsername.text.toString()
+        val password = fieldPassword.text.toString()
 
-        val response = apiService.login(
-            LoginRequest(Credentials(username, password))
-        ).await()
+        val request = apiService.login(
+            LoginRequest(
+                Credentials(username, password)
+            )
+        )
+        val response: LoginSuccessResponse
+
+        try {
+            response = request.await()
+        } catch (e: HttpException) {
+            hideLoading()
+            Toast.makeText(context, "Incorrect login or password! Try again", Toast.LENGTH_SHORT).show()
+            Log.e("LOGIN", "$e: ${e.message()}")
+            return@launch
+        }
 
         userDao.insert(
             User(response.userId, username, response.apiToken)
