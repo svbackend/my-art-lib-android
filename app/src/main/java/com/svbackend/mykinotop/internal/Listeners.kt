8699fc78785473ed4b1e753ef754e42b2d211246
Valentyn Saik <svbackend22@gmail.com>
2019-07-27 21:49:50 +0200

    Implemented few popular listeners

--- app/src/main/java/com/svbackend/mykinotop/internal/Listeners.kt
+++ app/src/main/java/com/svbackend/mykinotop/internal/Listeners.kt
@@ -1,2 +1,36 @@
 package com.svbackend.mykinotop.internal
 
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+
+class TextChanged(
+    private val lambda: (Editable) -> Unit
+) : TextWatcher {
+    override fun beforeTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {}
+    override fun onTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {}
+
+    override fun afterTextChanged(editable: Editable) {
+        lambda(editable)
+    }
+}
+
+class OnFocus(
+    private val lambda: () -> Unit
+) : View.OnFocusChangeListener {
+    override fun onFocusChange(v: View?, hasFocus: Boolean) {
+        if (hasFocus) {
+            lambda()
+        }
+    }
+}
+
+class OnFocusOut(
+    private val lambda: () -> Unit
+) : View.OnFocusChangeListener {
+    override fun onFocusChange(v: View?, hasFocus: Boolean) {
+        if (!hasFocus) {
+            lambda()
+        }
+    }
+}
